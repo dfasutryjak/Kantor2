ca815ac748eb7050250d76f2d5edafe606d03fbd
Damian Faustryjak <dfaustryjak@hotmail.com>
2016-03-12 23:26:48 +0100

    test

---
+++ Kantor2/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Kantor2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void home_click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            this.Frame.Navigate(typeof(Pages.home));
+        }
+
+        private void history_click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            this.Frame.Navigate(typeof(Pages.history));
+        }
+
+        private void home_tapped(object sender, TappedRoutedEventArgs e)
+        {
+            TextBlock b = (TextBlock)sender;
+            this.Frame.Navigate(typeof(Pages.home));
+        }
+
+        private void history_tapped(object sender, TappedRoutedEventArgs e)
+        {
+            TextBlock b = (TextBlock)sender;
+            this.Frame.Navigate(typeof(Pages.history));
+
+        }
+        
+        }
+}
---
+++ Kantor2/Pages/home.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Kantor2.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class home : Page
+    {
+        public home()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void home_click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            this.Frame.Navigate(typeof(Pages.home));
+        }
+
+        private void history_click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            this.Frame.Navigate(typeof(Pages.history));
+        }
+
+        private void home_tapped(object sender, TappedRoutedEventArgs e)
+        {
+            TextBlock b = (TextBlock)sender;
+            this.Frame.Navigate(typeof(Pages.home));
+        }
+
+        private void history_tapped(object sender, TappedRoutedEventArgs e)
+        {
+            TextBlock b = (TextBlock)sender;
+            this.Frame.Navigate(typeof(Pages.history));
+
+        }
+
+    }
+}
